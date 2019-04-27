ea68d2bac30a487e501e98b06311f3fe987c1cba
Johannes Westlund <johannes.westlund@cybercom.com>
2019-04-27 22:13:49 +0200

    Handle state for values in Calculator component via redux flow

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { makeStyles } from '@material-ui/styles';
 
+const reduxAction = {
+  changeOperator: 'CHANGE_OPERATOR',
+  changeValue: 'CHANGE_VALUE'
+};
+
 const Calculator = props => {
+  const [state, dispatch] = useReducer(reducer, { values: [0, 0, 0] });
+
   const styles = makeStyles({
     calculator: {
       display: 'flex'
     }
   });
   return (
-    <div class={styles().calculator}>
-      <InputBox />
-      <InputBox />
-      <InputBox />
+    <div className={styles().calculator}>
+      {state.values.map((value, index) => (
+        <InputBox key={index} index={index} value={value} dispatch={dispatch} />
+      ))}
     </div>
   );
 };
 
+const reducer = (state, action) => {
+  console.log(action.type === reduxAction.changeValue);
+  switch (action.type) {
+    case reduxAction.changeValue:
+      let newValues = state.values;
+      if (!isNaN(action.value) || action.value === '')
+        newValues[action.index] = action.value;
+      return { values: newValues };
+    default:
+      throw new Error();
+  }
+};
+
 const InputBox = props => {
+  const onValueChange = e => {
+    props.dispatch({
+      type: reduxAction.changeValue,
+      index: props.index,
+      value: e.target.value
+    });
+  };
+
   const styles = makeStyles({
     inputBox: {
       margin: '10px',
@@ -34,8 +62,14 @@ const InputBox = props => {
     }
   });
   return (
-    <div class={styles().inputBox}>
-      Value: <input type="text" class={styles().inputField} />
+    <div className={styles().inputBox}>
+      Value {props.index}:{' '}
+      <input
+        type="text"
+        className={styles().inputField}
+        value={props.value}
+        onChange={onValueChange}
+      />
     </div>
   );
 };
